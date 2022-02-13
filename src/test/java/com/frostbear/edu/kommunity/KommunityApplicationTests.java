d4d622420745bd610230418bbf3f02f647261898
Daniel Koch <k@kdb.gmbh>
2022-02-13 08:14:23 +0100

    Add framework

---
+++ src/test/java/com/frostbear/edu/kommunity/KommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frostbear.edu.kommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
